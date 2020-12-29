fb93345c4253864e782f74e2d298b2bf76d5b3e7
lempiji <lempiji@gmail.com>
2020-12-30 05:04:19 +0900

    Add example for process

--- src/d/source/app.d
+++ src/d/source/app.d
@@ -3,6 +3,7 @@ import mylib;
 void main()
 {
 	run_create();
+	run_process();
 }
 
 void run_create()
@@ -16,4 +17,19 @@ void run_create()
 	writeln(text);
 	writeln(array[]);
 	writeln(*pointer.get());
+}
+
+void run_process()
+{
+	import core.lifetime;
+	import core.stdcpp.memory;
+	import core.stdcpp.vector;
+
+	auto text = cppstring("Hello, C++!");
+	auto array = vector!int([10, 20, 30]);
+	auto pointer = make_unique!int(100);
+
+	process_string(text);
+	process_vector(array);
+	process_pointer(move(pointer));
 }
